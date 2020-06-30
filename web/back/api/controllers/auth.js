c738a600551d4972f6824a5cd8dba3b17eebf562
Demba <m.demba.dia@gmail.com>
2020-06-30 22:05:07 +0200

    update controller register and login

--- web/back/api/controllers/auth.js
+++ web/back/api/controllers/auth.js
@@ -1,50 +1,52 @@
 const passport = require('passport');
 const mongoose = require('mongoose');
 const User = require('./../models/user');
-const  bcrypt = require("bcryptjs");
 
-module.exports.register = function(req, res) {
-    
-     //create new user using schema
-     let newUser = new User({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 8)
-  });
+module.exports.register = (req, res) => {
   
-    newUser.save(function(err) {
-      var token;
-      token = newUser.generateJwt();
+  // create new user using schema
+  let newUser = new User({
+  firstName: req.body.firstName,
+  lastName: req.body.lastName,
+  email: req.body.email,
+  });
+
+  // hash your password
+  newUser.setPassword(req.body.password);
+
+  // save user in db
+  newUser.save(function(err) {
+    var token;
+    token = newUser.generateJwt();
+    res.status(200);
+    res.json({
+      "token" : token
+    });
+  });
+};
+
+module.exports.login = (req, res) => {
+
+  passport.authenticate('local', function(err, user, info) {
+    var token;
+
+    // if Passport throws/catches an error
+    if (err) {
+      res.status(404).json(err);
+      return;
+    }
+
+    // if a user is found
+    if(user){
+      token = user.generateJwt();
       res.status(200);
       res.json({
-        token : token
+        "token" : token
       });
-    });
-  };
+    } else {
+      // If user is not found
+      res.status(401).json(info);
+    }
+  })(req, res);
 
-  module.exports.login = function(req, res) {
-
-    passport.authenticate('local', function(err, user, info){
-      var token;
-  
-      // If Passport throws/catches an error
-      if (err) {
-        res.status(404).json(err);
-        return;
-      }
-  
-      // If a user is found
-      if(user){
-        token = newUser.generateJwt();
-        res.status(200);
-        res.json({
-          "token" : token
-        });
-      } else {
-        // If user is not found
-        res.status(401).json(info);
-      }
-    })(req, res);
-  
-  };
+};
